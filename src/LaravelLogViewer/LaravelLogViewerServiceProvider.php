8076a88c93d3d0e32e7e98bdce0189da55c31c4f
Tjoosten <Topairy@gmail.com>
2018-05-03 17:00:30 +0200

    Implement docblocks

--- src/LaravelLogViewer/LaravelLogViewerServiceProvider.php
+++ src/LaravelLogViewer/LaravelLogViewerServiceProvider.php
@@ -17,37 +17,33 @@ use Illuminate\Support\ServiceProvider;
 class LaravelLogViewerServiceProvider extends ServiceProvider
 {
     const CONFIG_PATH = __DIR__ . '/../config/laravel-log-viewer.php';
+    const VIEWS_PATH  = __DIR__ . '/../views';
 
-    const VIEWS_PATH = __DIR__ . '/../views';
-
-    public function boot()
+    /**
+     * Bootstrap any application services.
+     * 
+     * @return void
+     */
+    public function boot(): void
     {
         $this->loadViewsFrom(self::VIEWS_PATH, 'laravel-log-viewer');
 
-        $this->publishes([
-            self::CONFIG_PATH => config_path('laravel-log-viewer.php'),
-        ], 'config');
-
-        $this->publishes([
-            self::VIEWS_PATH =>
-                resource_path('views/vendor/laravel-log-viewer'),
-        ], 'views');
+        $this->publishes([self::CONFIG_PATH => config_path('laravel-log-viewer.php')], 'config');
+        $this->publishes([self::VIEWS_PATH  => resource_path('views/vendor/laravel-log-viewer')], 'views');
     }
 
+    /**
+     * Register bindings in the container.
+     * 
+     * @return void
+     */
     public function register()
     {
-        $this->mergeConfigFrom(
-            self::CONFIG_PATH,
-            'laravel-log-viewer'
-        );
+        $this->mergeConfigFrom(self::CONFIG_PATH, 'laravel-log-viewer');
 
-        $this->app->bind('log-viewer', function () {
+        $this->app->bind('log-viewer', function (): LaravelLogViewer {
             $baseDir = Config::get('laravel-log-viewer.base_dir');
-
-            return new LaravelLogViewer(
-                $baseDir ?: storage_path('logs'),
-                Config::get('laravel-log-viewer.max_file_size')
-            );
+            return new LaravelLogViewer($baseDir ?: storage_path('logs'), Config::get('laravel-log-viewer.max_file_size'));
         });
     }
 }
